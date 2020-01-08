5695ab1d8efaf32687914785c79cdd026c414a18
Domokos László <domokoslaci94@gmail.com>
2020-01-08 23:08:31 +0100

    Integrated hello and exclamation-mark clients.

--- src/main/kotlin/hu/veres/domokos/titlegenerator/controller/HelloWorldController.kt
+++ src/main/kotlin/hu/veres/domokos/titlegenerator/controller/HelloWorldController.kt
@@ -2,13 +2,20 @@ package hu.veres.domokos.titlegenerator.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import hu.veres.domokos.exclamation.mark.ExclamationMarkClient
+import hu.veres.domokos.titlegenerator.model.HelloResponse
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
+import org.springframework.web.reactive.function.client.WebClient
 
 @RestController
-class HelloWorldController {
+class HelloWorldController(val exclamationMarkClient: ExclamationMarkClient, val webClient: WebClient) {
 
     @GetMapping("hello" )
-    fun hello(): String{
-        return "Hello World!";
+    fun hello(): String {
+        val exclamationMark = exclamationMarkClient.exclamationMark.message;
+        val hello = webClient.get().uri("http://localhost:8082/hello").retrieve().bodyToMono(HelloResponse::class.java).block();
+
+        return hello!!.message + " world" + exclamationMark;
     }
 
 }
